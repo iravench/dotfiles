4b2e08003efc048d1253ee89dea8a37e84c29b2a
iravench <iravench@users.noreply.github.com>
2018-01-30 17:19:37 +1100

    change to use chrome headless

--- ohmyzsh/locomote.plugin.zsh
+++ ohmyzsh/locomote.plugin.zsh
@@ -1,16 +1,22 @@
 # cbt
-alias cbt_lc='NODE_PORT=4000 npm start'
-alias cbt_lcm='MOCK=1 grunt db:init && MOCK=1 NODE_PORT=4000 npm start'
-alias cbt_te='TZ=utc NODE_ENV=test'
-alias cbt_tec='cbt_te MOCK_UAPI_PORT=23050 MOCK_IDENTITY_PORT=23080 SYSTEM=APP SELENIUM_WINDOW_SIZE=2560x1440 SELENIUM_PORT=4444'
-alias cbt_m='cbt_te grunt db:init && cbt_te mocha'
-alias cbt_m2='cbt_te mocha'
-alias cbt_mc='cbt_tec grunt db:init && cbt_tec mocha'
-alias cbt_mc2='cbt_tec mocha'
 alias cbt_np='export PATH="$PATH:./node_modules/.bin"'
-alias cbt_cs='java -jar -Dwebdriver.gecko.driver=/Users/rchen/Downloads/geckodriver /Users/rchen/Downloads/selenium-server-standalone-3.4.0.jar'
-alias cbt_prod_db='ssh cbt-jump "ssh cbt-production-app1 \"./cbt_dump_db\" " | gunzip | psql -U postgres cbt_production'
 alias cbt_reset='npm install && grunt && grunt db:init'
+
+alias cbt_l='TZ=utc NODE_PORT=4000'
+alias cbt_lc='cbt_l npm start'
+alias cbt_lcm='cbt_l MOCK=1 grunt db:init && MOCK=1 cbt_lc'
+
+alias cbt_t='TZ=utc NODE_ENV=test NODE_PORT=3016 DEBUG=1'
+alias cbt_d='cbt_t grunt db:init'
+alias cbt_m='cbt_t mocha'
+alias cbt_md='cbt_d && cbt_m'
+
+alias chrome_headless='SELENIUM_BROWSER=chrome SELENIUM_BROWSER_HEADLESS=true SELENIUM_BROWSER_BINARY=/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+alias cbt_mc='chrome_headless cbt_m'
+
+alias cbt_mcd='chrome_headless cbt_t node --inspect node_modules/mocha/bin/_mocha'
+
+alias cbt_prod_db='ssh cbt-jump "ssh cbt-production-app1 \"./cbt_dump_db\" " | gunzip | psql -U postgres cbt_production'
 
 # tmp
 alias tmp_db='SKIP_MIGRATE=1 script/db --user=root travel_management_platform_development'
